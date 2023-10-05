67dc08c78ee3ac262ca016fdbeaa1475b481563e
Frerik Koch <fkoch@posteo.de>
2023-10-05 15:29:49 +0200

    Implemented IDictionaryExtensions.AddOrExtendCollection Added some content to readme

---
+++ FkUtil/FkUtil.Test/Extensions/IDictionaryExtensionsTest.cs
@@ -0,0 +1,30 @@
+﻿using FkUtil.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FkUtil.Test.Extensions
+{
+
+    /// <summary>
+    /// Tests for <see cref="IDictionaryExtensions"/>
+    /// </summary>
+    [TestClass]
+    public class IDictionaryExtensionsTest
+    {
+        [TestMethod]
+        public void AddOrExtendCollectionTest() { 
+
+            var dictUnderTest = new Dictionary<string,List<string>>();
+
+            //Test empty add
+            dictUnderTest.AddOrExtendCollection("firstKey", "firstEntry");
+
+            Assert.AreEqual(dictUnderTest["firstKey"][0], "firstEntry");
+
+            //Test add on existing key
+            dictUnderTest.AddOrExtendCollection("firstKey", "secondEntry");
+
+            CollectionAssert.AreEquivalent(dictUnderTest["firstKey"], new List<string> { "firstEntry", "secondEntry" });
+
+        }
+    }
+}
---
+++ FkUtil/FkUtil/Extensions/IDictionaryExtensions.cs
@@ -0,0 +1,31 @@
+﻿namespace FkUtil.Extensions
+{
+    /// <summary>
+    /// This class contains extension methods for <see cref="IDictionary{TKey, TValue}"/> 
+    /// </summary>
+    public static class IDictionaryExtensions
+    {
+        /// <summary>
+        /// Adds a value to a collection in a dictionary.
+        /// If the dictionary already contains the key the collection is extended
+        /// If the dictionary does not contain the key, the collection is created beforhand
+        /// </summary>
+        /// <typeparam name="TKey">Type of the dicitionary key (or index)</typeparam>
+        /// <typeparam name="TCollection">Type of the collection the dictionary indexes</typeparam>
+        /// <typeparam name="TValue">Type of the values inside the collections</typeparam>
+        /// <param name="dict">The dictionary to add to</param>
+        /// <param name="key">The key to add the value to</param>
+        /// <param name="newVal">The new value</param>
+        public static void AddOrExtendCollection<TKey,TCollection,TValue>(this IDictionary<TKey,TCollection> dict,TKey key, TValue newVal) where TCollection : ICollection<TValue>, new()
+        {
+            if (!dict.TryGetValue(key, out var collection))
+            {
+                collection = new TCollection();
+                dict.Add(key, collection);
+            }
+
+            collection.Add(newVal);
+        }
+
+    }
+}
